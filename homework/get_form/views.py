86c85914d225de9dcc29e6cb25c9fc5728c09ed5
ilya_popov <il_pop@mail.ru>
2023-11-14 17:01:33 +0300

    def.index получаем get запросы

--- homework/get_form/views.py
+++ homework/get_form/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 
+# f_name1=value1&f_name2=value2
+
 # Create your views here.
 
 def index(request):
-    result = render(request=request, template_name='get_form/index.html')
+    print(request.method)
+    res = dict()
+    if request.method == 'GET':
+        res = request.GET
+    elif request.method == 'GET':
+        pass
+    data = {'request': res}
+    result = render(request=request,
+                    template_name='get_form/index.html',
+                    context=data)
     return result
